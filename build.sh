0fa35e0d24eac7983684ca5975b177c3d2e17e83
Nathan Douglas <github@darkdell.net>
2022-05-27 18:22:13 -0400

    Updates build.sh.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ca65 code/deepdown.asm 
-ld65 deepdown.o -t nes -o build/deepdown.nes
+ca65 code/deepdown.asm -o build/deepdown.o;
+ld65 build/deepdown.o -t nes -o build/deepdown.nes;
